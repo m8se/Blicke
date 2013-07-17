ee49a8210b0046ea676008a4b6eba4429ce8e7c7
jon <john@john-VPCEH2D0E>
2013-07-17 13:44:57 +0200

    DyadGUI v0.2

--- additional/python-gui/main.py
+++ additional/python-gui/main.py
@@ -2,18 +2,32 @@ from Tkinter import *
 import pickle
 import time
 import tkFont 
+from numpy.distutils.exec_command import exec_command
 
 class App:
     list;
     def init_popup(self,root):
+        
         self.popup = Menu(root, tearoff=0)
-        self.popup.add_command(label="Diagramm") # , command=next) etc...
+        self.popup.add_command(label="Diagramm",command=self.showDyad) # , command=next) etc...
         self.popup.add_command(label="XCorr")
         self.popup.add_separator()
         self.popup.add_command(label="Home")
-
+    
+    def showClusterProb(self):
+        exec_command("cd ..;python ")   
+    def showClusterStaytime(self):
+        exec_command("cd ..;python staytime.py")
+    def showClusterCorr(self):
+        exec_command("cd ../correl;python correl.py")
+    def showClusterXCorr(self):
+        exec_command("cd ..;python xcorrel_all.py")
+    def showDyad(self):
+        exec_command("cd ..;python vis_dyade.py "+str(self.ids[self.cur]))
     def do_popup(self,event):
         # display the popup menu 
+        self.cur=self.list.nearest(event.y)
+        print self.cur
         self.popup.tk_popup(event.x_root, event.y_root,1)
                 
     def party(self,event):
@@ -28,7 +42,7 @@ class App:
 
         dyade_num=len(zeitreihen)
         f=open("../../Daten/id_lst.data");
-        ids=pickle.load(f)
+        self.ids=pickle.load(f)
         
         # Aufbau der GUI
         
@@ -55,7 +69,7 @@ class App:
         scrollbar=Scrollbar(f4,orient=VERTICAL)
         self.list=Listbox(f4,font=("Arial", 10),yscrollcommand=scrollbar.set,selectmode=MULTIPLE,selectbackground="red",selectforeground="white")
         scrollbar.config(command=self.list.yview)
-        for item in ids:
+        for item in self.ids:
             self.list.insert(0,"Dyade "+item)
         self.list.pack(fill=Y,side=LEFT)
         scrollbar.pack(side=LEFT,fill=Y)
@@ -64,7 +78,7 @@ class App:
         self.current=self.list.curselection()
         self.poll()
         
-        b_reset=Button(f3,text="Zuruecksetzen",command=self.list.selection_clear(FIRST,LAST))
+        b_reset=Button(f3,text="Zuruecksetzen",command=self.list.selection_clear(0))
         b_reset.pack(fill=BOTH)
         
         f5=LabelFrame(f2,text="Optionen")
@@ -75,13 +89,13 @@ class App:
         b_auf=Button(f5,text="Clusterung nach Aufenthaltswahrscheinlichkeit")
         b_auf.pack(fill=BOTH,expand=1)
         
-        b_state_change_rate=Button(f5,text="Clusterung nach Zustandsuebergangsrate")
+        b_state_change_rate=Button(f5,text="Clusterung nach Zustandsuebergangsrate",command=self.showClusterStaytime)
         b_state_change_rate.pack(fill=BOTH,expand=1)
         
-        b_corr=Button(f5,text="Clusterung nach Korrelationskoeffizient")
+        b_corr=Button(f5,text="Clusterung nach Korrelationskoeffizient",command=self.showClusterCorr)
         b_corr.pack(fill=BOTH,expand=1)
         
-        b_xcorr=Button(f5,text="Clusterung mit Kreuzkorrelation")
+        b_xcorr=Button(f5,text="Clusterung mit Kreuzkorrelation",command=self.showClusterXCorr)
         b_xcorr.pack(fill=BOTH,expand=1)
         self.init_popup(f2)
         self.list.bind("<Button-3>", self.do_popup)
