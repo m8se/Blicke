729e3fe6136fe722c3a1f08367dc4247e9815551
David <klehr@uni-potsdam.de>
2013-10-17 16:34:49 +0200

    Korrektur der Rückgabe Fkt

--- DAO.py
+++ DAO.py
@@ -26,17 +26,22 @@ class DAO:
     def req_rel_auf(self,reqs,stress):    
         self.stress_lst=[]
         for i in ([3,2,1,0]):
+            print i, stress
             if stress>=2**i:
                 self.stress_lst.append(i)
                 stress-=2**i
-            self.ans=zeros([len(reqs),len(self.stress_lst),16])
-            self.req_it=0 #anzahl der bearbeiteten anfragen
-            self.ans_it=0 #anzahl der bearbeiteten ids
-            for req in reqs:
-                self.id_nr=self.id_lst.index(str(req))
-            for self.stress in self.stress_lst:
-                self.ans[self.ans_it,:16]=self.rel_auf[self.id_nr,self.stress,:]
-                return self.ans[:]
+        self.ans=zeros([len(reqs),len(self.stress_lst),16])
+        self.req_it=0 #anzahl der bearbeiteten anfragen
+        for req in reqs:
+            self.id_nr=self.id_lst.index(str(req))
+            self.st_it=0
+            print "hey"
+            for self.st in self.stress_lst:
+                print "ho"
+                self.ans[self.req_it,self.st_it,:16]=self.rel_auf[self.id_nr,self.st,:]
+                self.st_it+=1
+            self.req_it+=1
+        return self.ans[:]
                     
     def req_zeitreihen(self,reqs,stress):
         self.stress_lst = []
@@ -47,11 +52,13 @@ class DAO:
         self.stress_lst.reverse
         self.ans = zeros([len(reqs),len(self.stress_lst), 4510])
         self.req_it = 0  # anzahl der bearbeiteten anfragen
-        self.ans_it = 0  # anzahl der bearbeiteten ids
         for req in reqs:
             self.id_nr=self.id_lst.index(str(req))
+            self.st_it=0
             for st in self.stress_lst:
-                self.ans[self.ans_it, :] = self.zeitreihen[self.id_nr, st, :]
-                self.ans_it+=1
+                self.ans[self.req_it,self.st_it, :] = self.zeitreihen[self.id_nr, st, :]
+                self.st_it+=1
+            self.req_it+=1
         return self.ans[:]
+	
     
